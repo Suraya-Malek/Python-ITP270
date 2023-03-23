5072646625b6d521b2bcf66ff85cd815f4164e28
Suraya_Malek <student@ip-10-1-117-164.ec2.internal>
2023-03-23 00:09:43 +0000

    My information in python script

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,13 @@
 #!/bin/python3
 
-#Print "Hello World"
-print("Hello World")
+#Taking user input for name, age, and degree program
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+degree_program = input("Enter your degree program: ")
+
+# Calculating the remaining aGE
+remaining_age = (age * 3)/2
+
+
+#Printing the message with string concatenation
+print(str("Hello, my name is " + name + ". My remainining age " + remaining_age + " years. My degree program is " + degree_program)) 
